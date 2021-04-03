bbf959746ae78abc2b7c37fe97868f565bd729be
AnJia <anjia0532@gmail.com>
2021-04-03 09:41:12 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -120,14 +120,14 @@ function main() {
 
   // 1、下载脚本
   download(js_url, './').then(res=>{
+    // 真快乐签到
+    sign_happy()
     // 2、替换cookie
     setupCookie()
     // 3、执行脚本
     exec(`node '${js_path}' >> '${result_path}'`);
     // 4、发送推送
     sendNotificationIfNeed() 
-    // 真快乐签到
-    sign_happy()
   }).catch((err)=>{
     console.log('脚本文件下载失败，任务中断！');
     fs.writeFileSync(error_path, err, 'utf8')
